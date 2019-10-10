9eaa10919d81695d8093f7708b1751017f609d31
hupeng <hupeng@rayeye.cn>
2019-10-10 19:04:31 +0800

    19/10/09 学习内容

--- springboot/src/test/java/com/myself/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/myself/springboot/SpringbootApplicationTests.java
@@ -3,14 +3,15 @@ package com.myself.springboot;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
