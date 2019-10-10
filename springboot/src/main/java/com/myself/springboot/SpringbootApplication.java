8a40cb2a80e110620395fc2f2b7835a70467b5ae
hupeng <hupeng@rayeye.cn>
2019-10-10 22:38:51 +0800

    19/10/10 学习内容

--- springboot/src/main/java/com/myself/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/myself/springboot/SpringbootApplication.java
@@ -3,9 +3,11 @@ package com.myself.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
