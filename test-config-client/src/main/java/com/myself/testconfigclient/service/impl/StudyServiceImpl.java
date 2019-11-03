43d047a6e70c5bf7e1173fb7f2b4339ead04af20
ByBye <1562998440@qq.com>
2019-11-03 22:52:35 +0800

    spring-cloud-bus study

--- test-config-client/src/main/java/com/myself/testconfigclient/service/impl/StudyServiceImpl.java
+++ test-config-client/src/main/java/com/myself/testconfigclient/service/impl/StudyServiceImpl.java
@@ -3,11 +3,13 @@ package com.myself.testconfigclient.service.impl;
 import com.myself.testconfigclient.service.StudyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope
 public class StudyServiceImpl implements StudyService {
 
     @Autowired
